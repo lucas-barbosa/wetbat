3447bff48c528b9a0daff4ba1ff94031b6a3364f
Lucas Barbosa <barbosa.lucash@gmail.com>
2023-07-11 15:26:18 -0300

    feat(backend): add usecase to get all quote

---
+++ backend/src/application/usecases/get-all-quotes.ts
@@ -0,0 +1,24 @@
+import { IQuoteRepository } from 'src/domain/repositories/quote-repository';
+import { Response } from '../utils/response';
+
+export class GetAllQuotes {
+  private readonly _quoteRepository: IQuoteRepository;
+
+  constructor (quoteRepository: IQuoteRepository) {
+    this._quoteRepository = quoteRepository;
+  }
+
+  async execute () {
+    const quotes = await this._quoteRepository.getAll();
+
+    return Response.success(quotes.map(quote => ({
+      customerName: quote.customerName,
+      from: quote.from,
+      destination: quote.destination,
+      departureDate: quote.departureDate,
+      returnDate: quote.returnDate,
+      passengerCount: quote.passengerCount,
+      modeOfTransportation: quote.modeOfTransportation
+    })));
+  }
+}
